246968b294e46e684e1d715a4ab817640edb814b
Amirhosein Asghari <amirhoseinasghari2000@gmail.com>
2022-09-30 17:13:33 +0330

    Routes created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+//components
+import Home from "./components/Home";
+import Header from "./components/Header";
+import ChannelDetails from "./components/ChannelDetails";
+import Video from "./components/Video";
+import SearchedVideo from "./components/SearchedVideo";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/channel/:id" element={<ChannelDetails />} />
+        <Route path="/video/:id" element={<Video />} />
+        <Route path="/search/:searchTerm" element={<SearchedVideo />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/SearchedVideo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SearchedVideo = () => {
+  return <div>SearchedVideo</div>;
+};
+
+export default SearchedVideo;
