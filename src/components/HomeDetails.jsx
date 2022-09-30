bc44b2f3564a1baa5f61bd2a73612c6f70fd8994
Amirhosein Asghari <amirhoseinasghari2000@gmail.com>
2022-09-30 17:25:48 +0330

    HomeDeails component : part 1

--- src/components/HomeDetails.jsx
+++ src/components/HomeDetails.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const HomeDetails = () => {
-  return <div>HomeDetails</div>;
+const HomeDetails = (props) => {
+  return (
+    <div>
+      {/* if there are any channel type in props,this section will return */}
+      {props.type === "channel" && (
+        <Link
+          to={`/channel/${props.channel.channelId}`}
+          className="flex items-center flex-col"
+        >
+          <div>
+            <img
+              className="rounded-full mb-5"
+              src={props.channel.avatar[1].url}
+              alt="avatar"
+            />
+          </div>
+          <p>{props.channel.title}</p>
+          <div className="flex flex-col items-center text-sm text-gray-400">
+            <p>{props.channel.stats.subscribersText}</p>
+            <p>{props.channel.stats.videos} videos</p>
+          </div>
+        </Link>
+      )}
+    </div>
+  );
 };
 
 export default HomeDetails;
